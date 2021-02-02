b385afe153a20dc0872d003aed28b4ad6f8ce379
DavideStenner <48441753+DavideStenner@users.noreply.github.com>
2021-02-02 11:28:48 +0100

    Scraping modification

--- Home-Scraper/src/scrape_utilities/url_scraper.py
+++ Home-Scraper/src/scrape_utilities/url_scraper.py
@@ -50,45 +50,34 @@ def scraper_single_url(args, data):
 
 def get_url_info(soup):
     
-    try:
-        #get tipologia
-        box_ = soup.find_all('dt', {'class': 'col-xs-12 col-sm-4'})
-
-        #cycle over each element when find tipologia get text
-        for i, x in enumerate(box_):
-            if x.get_text() =='Tipologia':
-                pos = i
-        
-        tipologia = soup.find_all('dd', {'class': 'col-xs-12 col-sm-8'})[pos].get_text()
-        del pos
-
-    except:
-        #assign na otherwise
-        tipologia = pd.NA
-        
-    try:
-        #find anno di costruzione by iterating over each element
-        box_ = soup.find_all('dt', {'class': 'col-xs-12 col-sm-8'})
-
-        for i, x in enumerate(box_):
-            if x.get_text() =='Anno di costruzione':
-                pos = i
-
-        #assign anno
-        anno_ = soup.find_all('dd', {'class': 'col-xs-12 col-sm-4'})[pos].get_text()
-        del pos
-    except:
-        #assign na otherwise
-        anno_ = pd.NA
-
-    try:
-        #get description
-        text_ = soup.find('div', {'class': 'col-xs-12 description-text text-compressed'}).get_text()
+    box_ = soup.find('dl', {'class': 'im-features__list'})
     
+    title_ = box_.find_all('dt', {'class': 'im-features__title'})
+    element_ = box_.find_all('dt', {'class': 'im-features__value'})
+    descrizione = box_.find('div', {'class': 'im-description__text js-readAllText'}).get_text().lower().strip()
+
+    try:
+        efficienza = box_.find('span', {'class': 'im-features__energy'}).get_text().lower().strip()
     except:
-        #assign na otherwise
-        text_ = pd.NA
-    
+        efficienza = pd.NA
+
+    for i in range(len(title_)):
+        
+        if title_[i].get_text().lower().strip() == 'anno di costruzione':
+            anno = element_[i].get_text().lower().strip()
+
+        elif title_[i].get_text().lower().strip() == 'stato':
+            stato = element_[i].get_text().lower().strip()
+
+        elif title_[i].get_text().lower().strip() == 'totale piani edificio':
+            totale_piani = element_[i].get_text().lower().strip()
+
+        elif title_[i].get_text().lower().strip() == 'contratto':
+            contratto = element_[i].get_text().lower().strip()
+
+        elif title_[i].get_text().lower().strip() == 'disponibilità':
+            disponibilità = element_[i].get_text().lower().strip()
+
     #return data frame
     df = pd.DataFrame({'Tipologia': [tipologia], 'Anno_Costruzione': anno_, 'Descrizione_Casa': text_})
 
