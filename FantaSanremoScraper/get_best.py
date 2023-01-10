070f55736da6ed2dc978bf9a9a8b71e9e39ae8cc
stenner <davide.stenner@gmail.com>
2023-01-10 10:54:28 +0100

    added calculation artists composition

--- FantaSanremoScraper/get_best.py
+++ FantaSanremoScraper/get_best.py
@@ -1,77 +1,59 @@
-#%%
+# %%
 import json
 import pandas as pd
 import numpy as np
+import cvxpy as cp
 
-with open('data/results_4999.json') as config_file:
+with open('mapping_artists.json') as mapping_file:
+    mapping = json.load(mapping_file)
+
+artists_mapping = mapping['artists_mapping']
+quotazioni = mapping['quotazioni']
+
+
+def get_artists_composition(
+    df: pd.DataFrame, 
+    num_baudi: int = 100, 
+    num_selection: int =5, solver=cp.ECOS_BB
+):
+    number_suggestion = df.shape[0]
+
+    selection = cp.Variable(number_suggestion, boolean=True)
+
+    score = cp.Constant(df['score'].values.tolist())
+    quotazione = cp.Constant(df['quotazione'].values.tolist())
+
+    problem_knapsack = sum(cp.multiply(score, selection))
+
+    constraint_list = [
+        sum(cp.multiply(selection, quotazione)) <= num_baudi,
+        sum(selection) == num_selection,
+    ]
+    fanta_problem = cp.Problem(
+        cp.Maximize(problem_knapsack), constraint_list
+    )
+
+    score = fanta_problem.solve(solver=cp.ECOS_BB)
+    selection_results = np.round(selection.value).astype(int)
+    results = df.loc[
+        selection_results==1
+    ]
+    assert results['quotazione'].sum() <= num_baudi
+
+    return results
+
+#%%
+with open('data/Pandora/results_1572.json') as config_file:
     results = json.load(config_file)
 
-remapper_dict = {
-    'artists/elodie.png': 'Elodie',
-    'artists/anna-oxa.png': 'Anna Oxa',
-    'artists/ariete.png': 'Ariete',
-    'artists/collazio.png': 'Colla Zio',
-    'artists/gianluca-grignani.png': 'Gianluca Grignani',
-    'artists/lazza.png': 'Lazza',
-    'artists/rosa-chemical.png': 'Rosa Chemical',
-    'artists/madame.png': 'Madame',
-    'artists/mr.png': 'Mr. Rain',
-    'artists/moda.png': 'Modà',
-    'artists/ultimo.png': 'Ultimo',
-    'artists/will.png': 'Will',
-    'artists/levante.png': 'Levante',
-    'artists/marco-mengoni.png': 'Marco Mengoni',
-    'artists/leo-gassmann.png': 'Leo Gassmann',
-    'artists/shari.png': 'Shari',
-    'artists/i-cugini-di-campagna.png': 'I Cugini di Campagna',
-    'artists/sethu.png': 'Sethu',
-    'artists/tananai.png': 'Tananai',
-    'artists/articolo-31.png': 'Articolo 31',
-    'artists/lda.png': 'LDA',
-    'artists/olly.png': 'OLLY',
-    'artists/gianmaria.png': 'gIANMARIA',
-    'artists/paola-e-chiara.png': 'Paola & Chiara',
-    'artists/colapesce-dimartino.png': 'Colapesce Dimartino',
-    'artists/giorgia.png': 'Giorgia',
-    'artists/mara-sattei.png': 'Mara Sattei',
-    'artists/coma-cose.png': 'Coma_Cose'
-}
+df = pd.DataFrame([[x, y] for x, y in results['frequency'].items()])
 
-quotazioni = {
-    'Anna Oxa': 18,
-    'Ariete': 21,
-    'Articolo 31': 21,
-    'Colapesce Dimartino': 20,
-    'Colla Zio': 16,
-    'Coma_Cose': 20,
-    'Elodie': 24,
-    'Gianluca Grignani': 20,
-    'gIANMARIA': 17,
-    'Giorgia': 25,
-    'I Cugini di Campagna': 21,
-    'Lazza': 22,
-    'LDA': 21,
-    'Leo Gassmann': 18,
-    'Levante': 20,
-    'Madame': 22,
-    'Mara Sattei': 21,
-    'Marco Mengoni': 26,
-    'Modà': 18,
-    'Mr. Rain': 19,
-    'OLLY': 16,
-    'Paola & Chiara': 22,
-    'Rosa Chemical': 19,
-    'Sethu': 16,
-    'Shari': 17,
-    'Tananai': 22,
-    'Ultimo': 27,
-    'Will': 16
-}
-# %%
-df = pd.DataFrame([[x, y] for x, y in results.items()])
 df.columns = ['artista', 'frequenza']
-df['artista'] = df['artista'].map(remapper_dict)
+df['artista'] = df['artista'].map(artists_mapping)
 df['quotazione'] = df['artista'].map(quotazioni)
-df['score'] = df['frequenza']/df['quotazione']
-# %%
-df.sort_values('score', ascending=False)
+df['norm_freq'] = df.groupby('quotazione')['frequenza'].transform('sum')
+
+df['score'] = df['frequenza']/df['norm_freq']
+
+#%%
+get_artists_composition(df)
